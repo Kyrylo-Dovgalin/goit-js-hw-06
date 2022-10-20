154ae3c057b1c6117dd5110387656778c6ae83d2
Kyrylo Dovgalin <kyrylo.dovgalin@gmail.com>
2022-10-20 15:31:08 +0300

    Solved Task#3

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,15 @@
+/*
+Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+
+<ul class="gallery"></ul>
+
+Используй массив объектов images для создания элементов <img> вложенных в <li>.
+Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+*/
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +24,43 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// const imgItems = [];
+// function createHTMLForGallery(items) {
+//   items.forEach(item => {
+//     const itemEl = document.createElement('li');
+//     const imgEl = document.createElement('img');
+//     imgEl.src = item.url;
+//     imgEl.alt = item.alt;
+//     itemEl.append(imgEl);
+//     imgItems.push(itemEl);
+//   });
+// }
+
+// createHTMLForGallery(images);
+
+//const galleryEl = document.querySelector('.gallery');
+//galleryEl.append(...imgItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.classList.add('gallery-flex');
+galleryEl.style.cssText = 'list-style:none;';
+
+function createHTMLForGallery(items) {
+  let html = '';
+  items.forEach(({ url, alt } = {}) => {
+    html += `<li><img src='${url} alt='${alt}' width= 360> </li>`;
+  });
+  return html;
+}
+
+// const imagesItemsEl = images
+//   .map(
+//     ({ url, alt }) =>
+//       `<li><img src=${url} alt=${alt} width='300' height='200'></li>`,
+//   )
+//   .join('');
+// galleryEl.insertAdjacentHTML('beforeend', imagesItemsEl);
+
+galleryEl.insertAdjacentHTML('beforeend', createHTMLForGallery(images));

c396169caccc29e7469631e46421520612bafb2d
Kyrylo Dovgalin <kyrylo.dovgalin@gmail.com>
2022-10-20 22:26:33 +0300

    Solved Task#6

--- js/task-06.js
+++ js/task-06.js
@@ -10,7 +10,7 @@ const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur', onBlur);
 
-function onBlur(event) {
+function onBlur() {
   inputEl.classList.add('invalid');
   inputEl.classList.remove('valid');
 

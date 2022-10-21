25901d78635cf06eb5a341e8b65dca1be2869b34
Kyrylo Dovgalin <kyrylo.dovgalin@gmail.com>
2022-10-21 09:38:04 +0300

    Solved Task#6

--- js/task-06.js
+++ js/task-06.js
@@ -11,11 +11,14 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onBlur);
 
 function onBlur() {
-  inputEl.classList.add('invalid');
-  inputEl.classList.remove('valid');
-
-  if (inputEl.value.length === +inputEl.dataset.length) {
+  if (!inputEl.value) {
+    inputEl.classList.remove('invalid');
+    inputEl.classList.remove('valid');
+  } else if (inputEl.value.length === +inputEl.dataset.length) {
     inputEl.classList.add('valid');
-    inputEl.classList.toggle('invalid');
+    inputEl.classList.remove('invalid');
+  } else {
+    inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid');
   }
 }
